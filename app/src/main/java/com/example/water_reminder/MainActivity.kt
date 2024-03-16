4c59592afb174565cf83a5c29ee88eef86e561bb
dhianapereira <dhianaslv@outlook.com>
2024-03-16 00:33:33 -0300

    feat: Create android project

---
+++ app/src/main/java/com/example/water_reminder/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.water_reminder
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.water_reminder.databinding.ActivityMainBinding
+
+class MainActivity : ComponentActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
